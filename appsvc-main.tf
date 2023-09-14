3ffe5a3bafa379dbcb1806b4e9efe76292323c4d
sherifsameh <mr_messenger10@hotmail.com>
2023-09-14 12:45:12 +0300

    Update appsvc-main.tf

--- appsvc-main.tf
+++ appsvc-main.tf
@@ -12,7 +12,6 @@ resource "azurerm_app_service_plan" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Linux"
-  reserved            = true
 
   sku {
     tier = "Basic"
@@ -21,12 +20,37 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "example-web-api"
+  name                = "example-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
+}
 
-  site_config {
-    linux_fx_version = "DOTNET|5.0"
+resource "azurerm_sql_server" "example" {
+  name                         = "example-sql-server"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Password12345!"  # Replace with a secure password
+
+  tags = {
+    environment = "dev"
   }
+}
+
+resource "azurerm_sql_database" "example" {
+  name                        = "example-sql-db"
+  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.example.location
+  server_name                 = azurerm_sql_server.example.name
+  requested_service_objective = "S0"  # Replace with the desired performance tier
+}
+
+resource "azurerm_app_service_connection_string" "example" {
+  name                = "example-db-connection"
+  app_service_id      = azurerm_app_service.example.id
+  type                = "SQLServer"
+  connection_string   = "Server=tcp:${azurerm_sql_server.example.fqdn},1433;Initial Catalog=${azurerm_sql_database.example.name};User Id=${azurerm_sql_server.example.administrator_login};Password=${azurerm_sql_server.example.administrator_login_password};"
+  in_use              = true
 }
